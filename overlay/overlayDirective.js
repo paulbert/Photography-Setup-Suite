07b88c4d1f9978d2a63078b7d60ebac42f7a4d3b
paulbert <paul.r.albert@gmail.com>
2016-02-23 16:32:28 -0500

    Clean overlay controller & directive
    Change isolate scope in directive.  Remove most functions in controller,
    which will now be included in parent controller.

--- overlay/overlayDirective.js
+++ overlay/overlayDirective.js
@@ -4,14 +4,11 @@
 	.directive('overlay', function() {
 		return {
 			scope: {
-				itemsToChange:'=items',
-				messageNum:'=message',
-				keyName:'@key',
-				tableName:'@table',
-				editVars:'=?evars',
-				action:'@',
-				listNum:'=list',
-				idName:'@?id'
+				messages:'=',
+				messageNum:'=number',
+				btnConfig:'=cfg',
+				// Function for action of overlay (cancel/back is in controller)
+				doFunction:'=func'
 			},
 			templateUrl: '/templates/edit/overlay.html',
 			controller:'overlayController',
